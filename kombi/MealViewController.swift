65ac006add75801534428c37174370f31083f965
William Oliveira de Lagos <william.lagos@icloud.com>
2018-08-11 16:03:07 -0300

    Added a new scene on storyboard and some new elements

--- kombi/MealViewController.swift
+++ kombi/MealViewController.swift
@@ -9,9 +9,8 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: Properties
-    @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var freighterNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var mapView: MKMapView!
@@ -21,7 +20,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         super.viewDidLoad()
 
         // Handle the text field's user input through delegate callbacks.
-        nameTextField.delegate = self
+//        nameTextField.delegate = self
         photoImageView.isUserInteractionEnabled = true
     }
 
@@ -33,7 +32,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     //MARK: Actions
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         // Hide the keyboard.
-        nameTextField.resignFirstResponder()
+//        nameTextField.resignFirstResponder()
         
         // UIImagePickerController is a view controller that lets a user pick media from their photo library.
         let imagePickerController = UIImagePickerController()
@@ -46,9 +45,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         present(imagePickerController, animated: true, completion: nil)
     }
     
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        freighterNameLabel.text = nameTextField.text
-    }
     
     //MARK: UIImagePickerControllerDelegate
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -75,6 +71,10 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         // Hide the keyboard.
         textField.resignFirstResponder()
         return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
     }
 }
 
