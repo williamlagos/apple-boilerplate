ef3a7fa969b1455586ea12437a66696c1159a2c6
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-27 18:20:43 -0300

    Cleared source tree with a new sample application for apple platforms

---
+++ Pomodoro/Pomodoro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pomodoro
+//
+//  Created by William Oliveira de Lagos on 27/07/20.
+//  Copyright © 2020 William Oliveira de Lagos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
